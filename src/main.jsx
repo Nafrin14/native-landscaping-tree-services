349db059fd34d802751ddb6020cad5efde4a2f90
Nafrin14 <nafrinnafrin49@gmail.com>
2026-07-18 02:06:28 +0530

    Add Netlify redirects

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/native-landscaping-tree-services">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
